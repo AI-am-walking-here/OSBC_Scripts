1ea83e3114d1fcc2f93c29fc7bf00269077f1a65
AI-am-walking-here <Daltonjens@gmail.com>
2023-06-19 14:48:29 +0800

    deposit_quantity(), WIP
    Added images for all the buttons, and clicking options for everything except for x

--- AI_Bots/AI_BotClass.py
+++ AI_Bots/AI_BotClass.py
@@ -19,6 +19,7 @@ class AI_BotClass(OSRSBot, metaclass=ABCMeta):
         super().__init__("AI_BotClass", bot_title, description)
         self.mouse_speed = "fast"
         self.pin = "1234" # Must be a string
+        self.bank_custom_quantity_set = False
 
 
     def close_bank(self, close='esc', logs=False,):
@@ -58,6 +59,15 @@ class AI_BotClass(OSRSBot, metaclass=ABCMeta):
 
    
     def enter_pin(self):
+        """
+        Enters 4-Digit PIN number.
+
+        Args:
+            self.pin = 4-Digit 'string', using numbers 0-9
+                            Example: '1234' or '1523'
+        
+        """
+
         # 'Bank' plugin - 'Keyboard Bankpin' option
         bank_pin_template_image = imsearch.BOT_IMAGES.joinpath("AI_BotClass_Images", "bank_pin_template.png")
         bank_pin_template = imsearch.search_img_in_rect(bank_pin_template_image, self.win.game_view)
@@ -84,9 +94,133 @@ class AI_BotClass(OSRSBot, metaclass=ABCMeta):
             pin_entered = True #Returns pin_entered True so the while loop will wait for the pin interface to close without looping again 
             return pin_entered
             
+    def deposit_quantity_set(self,quantity, x=14):
+        """
+        Leaves the bank menu using 'ESC' key or top-right '[X]'.
+
+        Args:
+            button: '1', '5', '10', 'X', or 'ALL' (case doesn't matter)
+            x : Custom Quantity Number (Default = 14") 
+                    
+        """
+        
+        button = quantity.lower()
+
+        bank_1_on_image = imsearch.BOT_IMAGES.joinpath("AI_BotClass_Images", "bank_1_on.png")
+        bank_1_off_image = imsearch.BOT_IMAGES.joinpath("AI_BotClass_Images", "bank_1_off.png")
+        bank_5_on_image = imsearch.BOT_IMAGES.joinpath("AI_BotClass_Images", "bank_5_on.png")
+        bank_5_off_image = imsearch.BOT_IMAGES.joinpath("AI_BotClass_Images", "bank_5_off.png")
+        bank_10_on_image = imsearch.BOT_IMAGES.joinpath("AI_BotClass_Images", "bank_10_on.png")
+        bank_10_off_image = imsearch.BOT_IMAGES.joinpath("AI_BotClass_Images", "bank_10_off.png")
+        bank_x_on_image = imsearch.BOT_IMAGES.joinpath("AI_BotClass_Images", "bank_x_on.png")
+        bank_x_off_image = imsearch.BOT_IMAGES.joinpath("AI_BotClass_Images", "bank_x_off.png")
+        bank_all_on_image = imsearch.BOT_IMAGES.joinpath("AI_BotClass_Images", "bank_all_on.png")
+        bank_all_off_image = imsearch.BOT_IMAGES.joinpath("AI_BotClass_Images", "bank_all_off.png")
+
+        if button == '1':
+            # Checks to see if 'off quantity' image needs to be turned on, Then clicks
+            bank_1_off = imsearch.search_img_in_rect(bank_1_off_image, self.win.game_view)
+            try:
+                self.mouse.move_to(bank_1_off.random_point(), mouseSpeed=self.mouse_speed)
+                self.mouse.click()
+                self.log_msg(f"Bank Quantity Set to {button}")
+
+            # Checks to see if 'on quantity' is already turned on, then returns
+            except AttributeError:
+                bank_1_on = imsearch.search_img_in_rect(bank_1_on_image, self.win.game_view)
+                if bank_1_on != None:                
+                    self.log_msg(f"Bank Quantity already set to {button}")
+
+                # If off or on image isn't found Close script    
+                else:
+                    self.log_msg(f"Couldn't find image of '{button}' quantity, ending script")
+                    raise SystemExit #Gracefully stops the script if pin isnt 4 characters
+
+        elif button == '5':
+            # Checks to see if 'off quantity' image needs to be turned on, Then clicks
+            bank_5_off = imsearch.search_img_in_rect(bank_5_off_image, self.win.game_view)
+            try:
+                self.mouse.move_to(bank_5_off.random_point(), mouseSpeed=self.mouse_speed)
+                self.mouse.click()
+                self.log_msg(f"Bank Quantity Set to {button}")
+
+            # Checks to see if 'on quantity' is already turned on, then returns
+            except AttributeError:
+                bank_5_on = imsearch.search_img_in_rect(bank_5_on_image, self.win.game_view)
+                if bank_5_on != None:                
+                    self.log_msg(f"Bank Quantity already set to {button}")
+
+                # If off or on image isn't found Close script    
+                else:
+                    self.log_msg(f"Couldn't find image of '{button}' quantity, ending script")
+                    raise SystemExit #Gracefully stops the script if pin isnt 4 characters
+                
+        elif button == '10':
+            # Checks to see if 'off quantity' image needs to be turned on, Then clicks
+            bank_10_off = imsearch.search_img_in_rect(bank_10_off_image, self.win.game_view)
+            try:
+                self.mouse.move_to(bank_10_off.random_point(), mouseSpeed=self.mouse_speed)
+                self.mouse.click()
+                self.log_msg(f"Bank Quantity Set to {button}")
+
+            # Checks to see if 'on quantity' is already turned on, then returns
+            except AttributeError:
+                bank_10_on = imsearch.search_img_in_rect(bank_10_on_image, self.win.game_view)
+                if bank_10_on != None:                
+                    self.log_msg(f"Bank Quantity already set to {button}")
+
+                # If off or on image isn't found Close script    
+                else:
+                    self.log_msg(f"Couldn't find image of '{button}' quantity, ending script")
+                    raise SystemExit #Gracefully stops the script if pin isnt 4 characters
+
+        elif button == 'all':
+            # Checks to see if 'off quantity' image needs to be turned on, Then clicks
+            bank_all_off = imsearch.search_img_in_rect(bank_all_off_image, self.win.game_view)
+            try:
+                self.mouse.move_to(bank_all_off.random_point(), mouseSpeed=self.mouse_speed)
+                self.mouse.click()
+                self.log_msg(f"Bank Quantity Set to {button}")
+
+            # Checks to see if 'on quantity' is already turned on, then returns
+            except AttributeError:
+                bank_all_on = imsearch.search_img_in_rect(bank_all_on_image, self.win.game_view)
+                if bank_all_on != None:                
+                    self.log_msg(f"Bank Quantity already set to {button}")
+
+                # If off or on image isn't found Close script    
+                else:
+                    self.log_msg(f"Couldn't find image of '{button}' quantity, ending script")
+                    raise SystemExit #Gracefully stops the script if pin isnt 4 characters
+
+
+
+        elif button == 'x':
+
+            #If the quantity was set earlier skip over function
+            if self.bank_custom_quantity_set == True:
+                self.log_msg(f"Custom bank quantity already set too {x}")
+
+            # Checks to see if 'off quantity' image needs to be turned on, Then clicks
+            else:
+                bank_x_off = imsearch.search_img_in_rect(bank_x_off_image, self.win.game_view)
+                try:
+                    self.mouse.move_to(bank_x_off.random_point(), mouseSpeed=self.mouse_speed)
+                    self.mouse.right_click()
+                    self.log_msg(f"Bank Quantity Set to {button}={x}")
+
+
+                    self.bank_custom_quantity_set = True
+                    return self.bank_custom_quantity_set
+
+                # If 'off quantity' isn't found and wasn't previously set stop running script
+                except AttributeError:
+                    self.log_msg(f"Couldn't find image of '{button}' quantity, ending script")
+                    raise SystemExit #Gracefully stops the script if pin isnt 4 characters
+    
+
 
         
-    
 
 
         
